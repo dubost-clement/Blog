4dd4f1f279af248d4ee763f7573a3edd8790dacd
dubost-clement <braunschweig-dubost@outlook.fr>
2018-05-30 00:48:32 +0200

    Affichage liste des reportages et  contenu des reportages

--- src/Controller/ReportageController.php
+++ src/Controller/ReportageController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Reportage;
+use App\Repository\ReportageRepository;
 
 class ReportageController extends Controller
 {
     /**
      * @Route("/reportage", name="reportage")
      */
-    public function index()
+    public function index(ReportageRepository $repo)
     {
+        $reportages = $repo->findall();
         return $this->render('reportage/index.html.twig', [
             'controller_name' => 'ReportageController',
+            'reportages' => $reportages
         ]);
     }
 
@@ -23,5 +27,15 @@ class ReportageController extends Controller
     public function home()
     {
         return $this->render('reportage/home.html.twig');
+    }
+
+    /**
+     * @Route("/reportage/{id}", name="reportage_show")
+     */
+    public function show(Reportage $reportage)
+    {
+        return $this->render('reportage/show.html.twig',[
+            'reportage' => $reportage
+        ]);
     }
 }
