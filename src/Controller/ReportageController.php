cd3fb6dcb5ae6964e78d13e0c77075586e38e9d7
dubost-clement <braunschweig-dubost@outlook.fr>
2018-05-29 22:37:58 +0200

    first commit

---
+++ src/Controller/ReportageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ReportageController extends Controller
+{
+    /**
+     * @Route("/reportage", name="reportage")
+     */
+    public function index()
+    {
+        return $this->render('reportage/index.html.twig', [
+            'controller_name' => 'ReportageController',
+        ]);
+    }
+
+    public function home()
+    {
+        return $this->render('reportage/home.html.twig');
+    }
+}
