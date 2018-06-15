f18a0a935c0e71b44939c9b8e6542c78d1cae0cd
dubost-clement <braunschweig-dubost@outlook.fr>
2018-06-15 13:05:40 +0200

    ajout formulaire de contact + envoi de mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,16 +4,41 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ContactController extends Controller
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function contactForm()
+    public function contactForm(Request $request, \Swift_Mailer $mailer)
     {
+        $form = $this->createFormBuilder()
+                    ->add('email', EmailType::class)
+                    ->add('message', TextareaType::class)
+                    ->add('envoyer', SubmitType::class)
+                    ->getForm();
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            
+            $data = $form->getData();
+            $message = (new \Swift_Message)
+                    ->setSubject('nouveau mail de votre site')
+                    ->setFrom($data['email'])
+                    ->setTo('braunschweig.dubost@gmail.com')
+                    ->setBody($form->getData()['message'],'text/plain')
+            ;
+            $mailer->send($message);
+        }
+
         return $this->render('contact/contact.html.twig',[
             'controller_name' => 'ContactController',
+            'emailForm' => $form->createView()
         ]);
     }
 }
